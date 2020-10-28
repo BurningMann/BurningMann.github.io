73069156f28591b357b6631eb7c1cdf6caa7a5eb
burning_man <sannnnna@bk.ru>
2020-10-28 15:26:09 +0300

    final

--- pravo/js/scripts.js
+++ pravo/js/scripts.js
@@ -1,75 +1,65 @@
-window.onload = function(){
-    $(".main_slider .slide_wrapper").slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        appendArrows: $('.main_slider .arrow_place'),
-        prevArrow: '<div class="main_arrow_prev"><img src="./img/svg/slider-arrow.svg"></div>',
-        nextArrow: '<div class="main_arrow_next"><img src="./img/svg/slider-arrow.svg"></div>',
-
-    })
-    $(".activities_slider").slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<div class="main_arrow_prev"><img src="./img/svg/activites-slider-arrow.svg"></div>',
-        nextArrow: '<div class="main_arrow_next"><img src="./img/svg/activites-slider-arrow.svg"></div>',
-        responsive: [
-            {
-              breakpoint: 1300,
-              settings: {
-                dots: true,
-                arrows: false,
-              }
-            }
-          ]
-    })
-    $(".reviews_slider_wrapper").slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        dots: true,
-        prevArrow: '<div class="main_arrow_prev"><img src="./img/svg/review-arrow.svg"></div>',
-        nextArrow: '<div class="main_arrow_next"><img src="./img/svg/review-arrow.svg"></div>',
-        responsive: [
-            {
-              breakpoint: 700,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-              }
-            }
-          ]
-
-    })
+window.onload = function() {
+	$(".main_slider .slide_wrapper").slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		appendArrows: $('.main_slider .arrow_place'),
+		prevArrow: '<div class="main_arrow_prev"><img src="./img/svg/slider-arrow.svg"></div>',
+		nextArrow: '<div class="main_arrow_next"><img src="./img/svg/slider-arrow.svg"></div>',
+	})
+	$(".activities_slider").slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<div class="main_arrow_prev"><img src="./img/svg/activites-slider-arrow.svg"></div>',
+		nextArrow: '<div class="main_arrow_next"><img src="./img/svg/activites-slider-arrow.svg"></div>',
+		responsive: [{
+			breakpoint: 1300,
+			settings: {
+				dots: true,
+				arrows: false,
+			}
+		}]
+	})
+	$(".reviews_slider_wrapper").slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		dots: true,
+		prevArrow: '<div class="main_arrow_prev"><img src="./img/svg/review-arrow.svg"></div>',
+		nextArrow: '<div class="main_arrow_next"><img src="./img/svg/review-arrow.svg"></div>',
+		responsive: [{
+			breakpoint: 700,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+			}
+		}]
+	})
 }
-
-
-$(".ham").click(function(){
-    $("body").addClass("hidden")
-    $(".modal_bg").fadeIn("fast");
-    $(".main_menu").addClass("open")
-    $(".main_menu.open .close .ham").click(function(){
-        $(this).closest(".open").removeClass("open");
-        $(".modal_bg").fadeOut("fast");
-        $("body").removeClass("hidden")
-    })
-    $("body").click(function(EO){
-        if(EO.target == $(".modal_bg")[0]){
-            $(".modal_bg").fadeOut()
-            $("body").removeClass("hidden")
-            $(this).find(".open").removeClass("open");
-        }
-    })
+$(".ham").click(function() {
+	$("body").addClass("hidden")
+	$(".modal_bg").fadeIn("fast");
+	$(".main_menu").addClass("open")
+	$(".main_menu.open .close .ham").click(function() {
+		$(this).closest(".open").removeClass("open");
+		$(".modal_bg").fadeOut("fast");
+		$("body").removeClass("hidden")
+	})
+	$("body").click(function(EO) {
+		if (EO.target == $(".modal_bg")[0]) {
+			$(".modal_bg").fadeOut()
+			$("body").removeClass("hidden")
+			$(this).find(".open").removeClass("open");
+		}
+	})
 })
-
-
-$(".open_arrow").click(function(){
-    $(this).toggleClass("open")
-    $(this).closest(".office_wrapper").find(".office_content").slideToggle()
+$(".open_arrow").click(function() {
+	$(this).toggleClass("open")
+	$(this).closest(".office_wrapper").find(".office_content").slideToggle()
 })
-$(".current_lang").click(function(){
-    $(this).toggleClass("open")
-    $(".lang_list").slideToggle().css("display","flex")
+$(".current_lang").click(function() {
+	$(this).toggleClass("open")
+	$(".lang_list").slideToggle().css("display", "flex")
 })
