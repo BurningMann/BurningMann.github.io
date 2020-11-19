09614ae9b8988ec5d4bb7af36ad2b9a49daabc8e
burning_man <sannnnna@bk.ru>
2020-11-19 21:06:56 +0300

    main

--- surprise-main/js/scripts.js
+++ surprise-main/js/scripts.js
@@ -1,7 +1,6 @@
 $(".dropp_element").click(function(){
   $(this).find(".dropp").slideToggle()
 })
-
 $(".filtred_buttons_wrapper .buttons li").click(function(){
   
   $(".filtred_buttons_wrapper .buttons li.active span").addClass("doted")
@@ -18,9 +17,14 @@ var currentSlide;
 var slidesCount;
 var sliderCounter = `<div class="slider_counter"></div>`;
 var updateSliderCounter = function(slick, currentIndex) {
+  if(slick.activeBreakpoint != null){
+    let breakepoint = slick.activeBreakpoint
+    slidesCount = slick.slideCount - slick.breakpointSettings[breakepoint].slidesToShow + 1;
+    
+  }else{
+    slidesCount = slick.slideCount - slick.originalSettings.slidesToShow + 1;
+  }
   currentSlide = slick.slickCurrentSlide() + 1;
-  slidesCount = slick.slideCount - slick.originalSettings.slidesToShow +1;
-  console.log($(sliderCounter))
   $slider.find(".slider_counter").html(`<span class="current_slide">${currentSlide}</span> / <span>${slidesCount}</span>`)
   if(currentSlide == slidesCount){
       $slider.find(".next").removeClass("active")
@@ -32,6 +36,7 @@ var updateSliderCounter = function(slick, currentIndex) {
   }else{
       $slider.find(".prev").addClass("active")
   }
+  
 };
 $slider.on('init', function(event, slick) {
   $slider.append(sliderCounter);
@@ -45,7 +50,23 @@ $slider.slick({
   slidesToScroll: 1,
   infinite: false, 
   prevArrow: '<div class="prev"></div>',
-  nextArrow: '<div class="next active"></div>'     
+  nextArrow: '<div class="next active"></div>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 5,
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite:true
+      }
+    },
+  ]     
   });
 }
 
@@ -57,8 +78,19 @@ let OfferCurrentSlide;
 var OfferSlidesCount;
 var sliderCounter = `<div class="slider_counter"></div>`;
 var updateSliderCounterr = function(slick, currentIndex) {
+  if(slick.activeBreakpoint != null){
+    let breakepoint = slick.activeBreakpoint
+    OfferSlidesCount = slick.slideCount - slick.breakpointSettings[breakepoint].slidesToShow + 1;
+    
+  }else{
+/*     if(slick.originalSettings.slidesToScroll > 0){
+      console.log(slick.slickCurrentSlide())
+      console.log(slick.originalSettings.slidesToScroll )
+      OfferCurrentSlide = slick.slickCurrentSlide() - slick.originalSettings.slidesToScroll
+    } */
+    OfferSlidesCount = slick.slideCount - slick.originalSettings.slidesToShow + 1;
+  }
   OfferCurrentSlide = slick.slickCurrentSlide() + 1;
-  OfferSlidesCount = slick.slideCount - slick.originalSettings.slidesToShow +1;
   $offer_slider.find(".slider_counter").html(`<span class="current_slide">${OfferCurrentSlide}</span> / <span>${OfferSlidesCount}</span>`)
   if(OfferCurrentSlide == OfferSlidesCount){
       $offer_slider.find(".next").removeClass("active")
@@ -80,11 +112,25 @@ $offer_slider.on('afterChange', function(event, slick, OfferCurrentSlide) {
   updateSliderCounterr(slick, OfferCurrentSlide);
 });
 $offer_slider.slick({
-  slidesToShow: 3,
+  slidesToShow: 2,
   slidesToScroll: 1,
   infinite: false, 
   prevArrow: '<div class="prev"></div>',
-  nextArrow: '<div class="next active"></div>'     
+  nextArrow: '<div class="next active"></div>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
   });
 }
 
@@ -92,5 +138,19 @@ $(".category_slider_wrapper").slick({
 slidesToShow: 8,
 slidesToScroll: 1,
 prevArrow: '<div class="prev"></div>',
-nextArrow: '<div class="next"></div>'     
+nextArrow: '<div class="next"></div>',
+responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 6,
+    }
+  },
+  {
+    breakpoint: 720,
+    settings: {
+      slidesToShow: 4,
+    }
+  },
+]     
 });
