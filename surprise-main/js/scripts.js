3f573551922f64b6270f23841be4eaed19d942d7
burning_man <sannnnna@bk.ru>
2020-12-16 21:05:59 +0300

    basket

--- surprise-main/js/scripts.js
+++ surprise-main/js/scripts.js
@@ -155,3 +155,88 @@ responsive: [
   }
 ]     
 });
+
+/* cart script */
+
+$(".cart_tabs_line .tab").click(function(){
+  if($(this).hasClass("active")){}
+  else{
+    let tab = $(this).data("tab")
+    $(".cart_tabs_line .tab.active").removeClass("active")
+    $(this).addClass("active")
+
+    $(".cart-wrap .tab_content.active").removeClass("active")
+    $("#"+tab).addClass("active")
+  }
+})
+
+$(".package_wrapper .package_radio").click(function(){
+  if($(this).closest(".package").hasClass("active")){}
+  else{
+    $(".package_wrapper .package.active").removeClass("active")
+    $(this).closest(".package").addClass("active")
+  }
+})
+
+$(".parrent").click(function(){
+  if($(".parrent.active").length > 0 && $(this).hasClass("active")){
+    
+    $(this).find(".drop").slideUp(500, "linear", function(){
+      $(this).closest(".parrent").removeClass("active")
+    })
+    return false
+  }
+  let current = $(this)
+  if($(".parrent.active").length > 0 ){
+    $(".parrent.active").find(".drop").slideUp(200, "linear", function(){
+      $(".parrent.active").removeClass("active")
+      $(current).find(".drop").slideDown()
+      $(current).addClass("active")
+     })
+    return false
+  }
+  if($(".parrent.active").length == 0 ){
+    $(this).find(".drop").slideDown()
+    $(this).addClass("active")
+    return false
+  }
+})
+
+$(".drop_element").click(function(EO){
+  EO.stopPropagation()
+  $(this).closest(".drop").slideUp(200, "linear", function(){
+    $(".parrent.active").removeClass("active")
+  })
+  $(this).closest(".drop").find(".current").removeClass("current")
+  $(this).addClass("current")
+  let to = $(this).data("to"),
+      value = $(this).find("span").text(),
+      color = $(this).find(".color").css("background")
+  $("."+to).value = value
+  $(this).closest(".parrent").find(".main_name").text(value)
+  $(this).closest(".parrent").find(".main_color").css("background", color)
+
+  /* for present */
+  if($(this).parent().hasClass("title_choise_list")){
+    
+    $(".present_sample .title").text($(this).find("span").text())
+  }
+  if($(this).hasClass("your_title")){
+    $(".my_title").css("display","block")
+    $(".textarea_title.disabel").removeClass("disabel")
+    $(".my_title").on('input', function () {
+     var $item = $(this),
+        value = $item.val();
+        $(".present_sample .title").text(value)
+    });
+  }else{
+    $(".my_title").css("display","none")
+    $(".textarea_title").addClass("disabel")
+  }
+
+})
+
+$(".present_name").click(function(){
+  let text = $(this).data("text")
+  $(this).closest(".basket-item").find(".name").text(text)
+})
